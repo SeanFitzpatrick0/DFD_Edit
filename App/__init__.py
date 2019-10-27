ebc2c62cf4e17a5c6ec1053e9123a1e6ba04ddf5
Sean Fitzpatrick <seanlkfitzpatrick@gmail.com>
2019-10-27 17:10:00 +0000

    Adding login & register forms

--- App/__init__.py
+++ App/__init__.py
@@ -2,6 +2,8 @@ from flask import Flask
 
 
 app = Flask(__name__)
+# TODO hard coded SK for development. Remove later
+app.config['SECRET_KEY'] = '214b91243903a5597c08d35bb65dacb7'
 
 
 from App import routes
--- App/routes.py
+++ App/routes.py
@@ -1,12 +1,34 @@
-from flask import render_template, url_for
+from flask import render_template, url_for, flash, redirect
+from App.forms import RegistrationForm, LoginForm
 from App import app
 
 
 @app.route('/')
 def home():
-    return render_template('home.html')
+    return render_template('home.html', title='Home')
 
 
 @app.route('/editor')
 def editor():
-    return render_template('editor.html')
+    return render_template('editor.html', title='Editor')
+
+
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+    form = RegistrationForm()
+
+    if form.validate_on_submit():
+        flash('Account created for {}.'.format(form.username.data), 'success')
+        return redirect(url_for('home'))
+
+    return render_template('register.html', title='Register', form=form)
+
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    form = LoginForm()
+
+    if form.validate_on_submit():
+        return redirect(url_for('home'))
+
+    return render_template('login.html', title='Log in', form=form)
